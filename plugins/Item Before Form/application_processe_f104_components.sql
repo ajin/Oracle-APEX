cdf3b20653347a6ba93424369d01e2754d6bff75
Ajin <ajin.tuladhar@gmail.com>
2014-10-18 11:15:57 +0200

    Initial application process export

---
+++ plugins/Item Before Form/application_processe_f104_components.sql
@@ -0,0 +1,137 @@
+set define off
+set verify off
+set feedback off
+WHENEVER SQLERROR EXIT SQL.SQLCODE ROLLBACK
+begin wwv_flow.g_import_in_progress := true; end;
+/
+ 
+ 
+--application/set_environment
+prompt  APPLICATION 104 - Unknown
+--
+-- Application Export:
+--   Application:     104
+--   Name:            Unknown
+--   Date and Time:   11:09 Saturday October 18, 2014
+--   Exported By:     AMTULADHAR
+--   Flashback:       0
+--   Export Type:     Component Export
+--   Manifest
+--     APP PROCESS: Item Before Form
+--   Manifest End
+--   Version:         4.2.5.00.08
+--   Instance ID:     65419405175823
+--
+-- Import:
+--   Using Application Builder
+--   or
+--   Using SQL*Plus as the Oracle user APEX_040200 or as the owner (parsing schema) of the application
+ 
+ 
+--       AAAA       PPPPP   EEEEEE  XX      XX
+--      AA  AA      PP  PP  EE       XX    XX
+--     AA    AA     PP  PP  EE        XX  XX
+--    AAAAAAAAAA    PPPPP   EEEE       XXXX
+--   AA        AA   PP      EE        XX  XX
+--  AA          AA  PP      EE       XX    XX
+--  AA          AA  PP      EEEEEE  XX      XX
+prompt  Set Credentials...
+ 
+begin
+ 
+  -- Assumes you are running the script connected to SQL*Plus as the Oracle user APEX_040200 or as the owner (parsing schema) of the application.
+  wwv_flow_api.set_security_group_id(p_security_group_id=>nvl(wwv_flow_application_install.get_workspace_id,4842312955135473));
+ 
+end;
+/
+
+begin wwv_flow.g_import_in_progress := true; end;
+/
+begin 
+
+select value into wwv_flow_api.g_nls_numeric_chars from nls_session_parameters where parameter='NLS_NUMERIC_CHARACTERS';
+
+end;
+
+/
+begin execute immediate 'alter session set nls_numeric_characters=''.,''';
+
+end;
+
+/
+begin wwv_flow.g_browser_language := 'en'; end;
+/
+prompt  Check Compatibility...
+ 
+begin
+ 
+-- This date identifies the minimum version required to import this file.
+wwv_flow_api.set_version(p_version_yyyy_mm_dd=>'2012.01.01');
+ 
+end;
+/
+
+prompt  Set Application ID...
+ 
+begin
+ 
+   -- SET APPLICATION ID
+   wwv_flow.g_flow_id := nvl(wwv_flow_application_install.get_application_id,104);
+   wwv_flow_api.g_id_offset := nvl(wwv_flow_application_install.get_offset,0);
+null;
+ 
+end;
+/
+
+-- C O M P O N E N T    E X P O R T
+-- Requires Application Express 2.2 or greater
+begin
+  wwv_flow_api.g_mode := 'REPLACE';
+end;
+/
+ 
+ 
+prompt Component Export: APP PROCESS 7058532520893743
+ 
+prompt  ...application processes
+--
+ 
+begin
+ 
+declare
+    p varchar2(32767) := null;
+    l_clob clob;
+    l_length number := 1;
+begin
+p:=p||'wwv_flow.g_form_painted := true;';
+
+wwv_flow_api.create_flow_process(
+  p_id => 7058532520893743 + wwv_flow_api.g_id_offset,
+  p_flow_id => wwv_flow.g_flow_id,
+  p_process_sequence=> 1,
+  p_process_point => 'BEFORE_HEADER',
+  p_process_type=> 'PLSQL',
+  p_process_name=> 'Item Before Form',
+  p_process_sql_clob=> p,
+  p_process_error_message=> '',
+  p_error_display_location=> 'ON_ERROR_PAGE',
+  p_process_when=> '',
+  p_process_when_type=> '',
+  p_process_comment=> '');
+end;
+ 
+null;
+ 
+end;
+/
+
+--commit;
+begin
+execute immediate 'begin sys.dbms_session.set_nls( param => ''NLS_NUMERIC_CHARACTERS'', value => '''''''' || replace(wwv_flow_api.g_nls_numeric_chars,'''''''','''''''''''') || ''''''''); end;';
+end;
+/
+set verify on
+set feedback on
+set define on
+prompt  ...done
+COMMIT;
